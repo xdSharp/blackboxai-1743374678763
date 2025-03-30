8f17fde0009cc4aa8ac95778a1673dbd4f0fba20
xdSharp <63095650+xdSharp@users.noreply.github.com>
2025-03-31 01:45:07 +0300

    Add SalesApp/App.xaml.cs 1743374706275

---
+++ SalesApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using SalesApp.Pages;
+using SalesApp.Services;
+
+namespace SalesApp;
+
+public partial class App : Application
+{
+    private readonly AuthService _authService;
+
+    public App(AuthService authService)
+    {
+        _authService = authService;
+        InitializeComponent();
+        SetupMainPage();
+    }
+
+    private void SetupMainPage()
+    {
+        var isAuthenticated = _authService.IsAuthenticated();
+        MainPage = isAuthenticated 
+            ? new AppShell() 
+            : new NavigationPage(new LoginPage());
+    }
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+        // Additional startup logic
+    }
+}
