aff146b1435e30a4b101e26753bf87be0b02334a
xdSharp <63095650+xdSharp@users.noreply.github.com>
2025-03-31 01:45:00 +0300

    Add SalesApp/Models/SalesData.cs 1743374699311

---
+++ SalesApp/Models/SalesData.cs
@@ -0,0 +1,8 @@
+namespace SalesApp.Models;
+public class SalesData
+{
+    public DateTime Date { get; set; }
+    public string ProductName { get; set; }
+    public decimal Amount { get; set; }
+    public int Quantity { get; set; }
+}
