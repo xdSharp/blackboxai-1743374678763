96dbf08795a561cc9e60b82c1a3bebe91e24791c
xdSharp <63095650+xdSharp@users.noreply.github.com>
2025-03-31 01:45:05 +0300

    Add SalesApp/AppShell.xaml.cs 1743374704485

---
+++ SalesApp/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace SalesApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+        
+        // Register routes
+        Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+        Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
+        Routing.RegisterRoute(nameof(SalesEntryPage), typeof(SalesEntryPage));
+    }
+}
